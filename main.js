01cde77e44dcde010db6254290bb80c7da35c009
johndward01 <johndward01@gmail.com>
2021-06-02 20:15:44 -0500

    Modal & Side-Nav

--- main.js
+++ main.js
@@ -7,3 +7,38 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  // let modals = document.getElementsByClassName("modal");
+
+  // for (let i = 0; i < modals.length; i++) {
+  //   const element = modals[i];
+  //   element.addEventListener("onclick", function () {
+      
+  //   })
+  // }
+
+  // Get the modal
+  var modal = document.getElementById("Tacobell");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("taco-btn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
